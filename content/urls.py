78df20e00a605311bbc711d33b2561cbe711a885
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-04 20:30:59 +0300

    Add static files directory path in settings.

--- content/urls.py
+++ content/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-  path('', views.login, name= 'Home'),
-  path('login/', views.index, name = 'Login' ),
-  path('register/')
+  path('', views.login, name= 'home'),
+  path('login/', views.index, name = 'login' ),
+  path('register/', views.register, name = 'register'),
+  path('profile/', views.profile, name = 'profile')
+
 ]
 
 if settings.DEBUG:
--- content/views.py
+++ content/views.py
@@ -12,3 +12,6 @@ def index (request):
 def register(request):
   return render(request, 'register.html')
 
+def profile(request):
+  return render(request,'profile.html')
+
