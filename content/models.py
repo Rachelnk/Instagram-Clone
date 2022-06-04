d764b0c5cc1d0cea5e4f5e1e81507e8d74af118a
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-04 17:07:18 +0300

    Add like class model.

--- content/models.py
+++ content/models.py
@@ -1,13 +1,16 @@
 from tkinter import CASCADE
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User')
     profile_pic = CloudinaryField('')
     bio = models.TextField()
     username = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
 
 class Post(models.Model):
   image = CloudinaryField('image')
@@ -15,14 +18,19 @@ class Post(models.Model):
   caption = models.TextField()
   comments = models.TextField()
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  author= models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
   created = models.DateTimeField(auto_now_add=True, null=True)
   # likes = 
 
 class Comment(models.Model):
-  comment = models.TextField()
+  comment = models.TextField(max_length=1000)
   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-  user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Like(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post,on_delete=models.CASCADE)
 
 
 
