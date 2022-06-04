ef7d96c5516590a050f574611dbe9f42f4269585
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-04 16:41:20 +0300

    Add database configurations in settings.py.

--- content/models.py
+++ content/models.py
@@ -1,17 +1,29 @@
+from tkinter import CASCADE
 from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-class Image(models.Model):
-  image = CloudinaryField('image')
-  name = models.CharField(max_length=60)
-  caption = models.TextField()
-  comments = models.TextField()
-  # likes = 
-
 class Profile(models.Model):
     profile_pic = CloudinaryField('')
     bio = models.TextField()
     username = models.CharField(max_length=60)
+
+class Post(models.Model):
+  image = CloudinaryField('image')
+  name = models.CharField(max_length=60)
+  caption = models.TextField()
+  comments = models.TextField()
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True, null=True)
+  # likes = 
+
+class Comment(models.Model):
+  comment = models.TextField()
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+  created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+
 
--- instagram/settings.py
+++ instagram/settings.py
@@ -81,9 +81,12 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'kiarie',
+        'PASSWORD': 'rayray'
+
+        }
 }
 
 
@@ -127,6 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Cloudinary configurations
 cloudinary.config( 
