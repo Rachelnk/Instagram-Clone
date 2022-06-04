90764b5f41a8e2a8e34b74029c3068a9228efd76
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-04 15:49:04 +0300

    Create models for images upload and user profile.

--- content/models.py
+++ content/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-
 # Create your models here.
+
+class Image(models.Model):
+  image = CloudinaryField('image')
+  name = models.CharField(max_length=60)
+  caption = models.TextField()
+  comments = models.TextField()
+  # likes = 
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('')
+    bio = models.TextField()
+    username = models.CharField(max_length=60)
+
