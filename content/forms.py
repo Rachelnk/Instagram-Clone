a2957e226ce945fab3b3ded64fab69390245ab93
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-07 11:03:04 +0300

    Create update user form.

---
+++ content/forms.py
@@ -0,0 +1,14 @@
+from django import forms 
+from django.contrib.auth.models import User
+from .models import Comment, Post, Profile
+
+class UpdateUserForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
--- content/views.py
+++ content/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='login')
+
 def login(request):
   return render (request, 'login.html')
-
+  
+@login_required(login_url='login')
 def index (request):
   return render (request, 'index.html')
 
