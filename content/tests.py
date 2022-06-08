0105d2276fb577f757ed23040692cb487a1f354c
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-08 19:29:08 +0300

    Create testcase for followers.

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Comment, Post, Follow, Like, Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+user = User.objects.get(id=1)
+
+class TestFollowers(TestCase):
+    def setUp(self):
+        self.new_user=User(first_name='Ray', last_name='Ray', username='Ray', email='ray@gmail.com',password='123')
+        self.new_user.save()
+        user=Follow(user='user',following=1)
+        user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(user, Follow))
