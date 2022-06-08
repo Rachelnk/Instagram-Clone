23235a4e7d10462412f14c1856d6ca20c17103ed
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-09 00:05:08 +0300

    Edit cloudinary configurations in settings.py file.

--- instagram/settings.py
+++ instagram/settings.py
@@ -142,9 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cloudinary configurations
 cloudinary.config( 
-  cloud_name = os.environ.get('CLOUD_NAME'),
-  api_key = os.environ.get('API_KEY'), 
-  api_secret = os.environ.get('API_SECRET'),
+  cloud_name = config('CLOUD_NAME'),
+  api_key = config('API_KEY'), 
+  api_secret = config('API_SECRET'),
   secure = True
 )
 # media configurations
