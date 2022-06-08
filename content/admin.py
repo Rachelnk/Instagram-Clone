61eafc48170cc917c9bc0c96c560adc3ede501be
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-08 08:25:20 +0300

    Register profile and post model in admin.py.

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Profile
 
 # Register your models here.
+admin.site.register(Post)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email_confirmed', 'date_created', 'date_updated')
+    search_fields = []
+    readonly_fields=('date_created', 'date_updated')
+
+admin.site.register(Profile, ProfileAdmin)
--- instagram/settings.py
+++ instagram/settings.py
@@ -148,5 +148,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login & Logout URLs
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/index/'
+# LOGIN_REDIRECT_URL = '/index/'
 LOGOUT_REDIRECT_URL = '/login/'
