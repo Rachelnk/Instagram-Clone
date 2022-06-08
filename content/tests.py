8a66bf9c73df68a0b4af7aac26bbbef077946b4f
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-08 19:32:51 +0300

    Create test case for post model class.

--- content/tests.py
+++ content/tests.py
@@ -19,3 +19,26 @@ class TestComment(TestCase):
     def setUp(self):
         self.new_comment=Comment(comment='comment', author=user, post=self.new_post)
         self.new_comment.save()
+
+class TestLike(TestCase):
+    def setUp(self):
+        self.new_user=User(first_name='John', last_name='Doe', username='user', email='user@gmail.com',password='user')
+        self.new_user.save()
+        user=Like(author=1, post=1)
+        user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(user,Like))
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.new_post=Post(image = "default.jpg", title="Title", caption='hello world', author=user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Post))
+
+    def test_save_image(self):
+        new_p=self.new_post
+        new_p.save_image()
+        posts=Post.get_posts()
+        self.assertTrue(len(posts)>0)
